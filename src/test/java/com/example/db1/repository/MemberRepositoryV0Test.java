32f2132624a1365e754063b9dfddeef7503030a4
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-29 22:06:56 +0900

    jdbc 개발 -수정, 삭제
    db 드라이버는 커넥션 객체를 생성해서 클라이언트에 반환한다.

--- src/test/java/com/example/db1/repository/MemberRepositoryV0Test.java
+++ src/test/java/com/example/db1/repository/MemberRepositoryV0Test.java
@@ -2,14 +2,14 @@ package com.example.db1.repository;
 
 import com.example.db1.domain.Member;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
-@Transactional
-@SpringBootTest
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -23,10 +23,19 @@ class MemberRepositoryV0Test {
         //findById
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
-
         /**
          * member 객체에 @Data 어노테이션이 붙어있기 때문에 equals 메소드가 생성되어 있다.
          */
-        Assertions.assertThat(findMember).isEqualTo(member);
+        assertThat(findMember).isEqualTo(member);
+
+        // update
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repository.delete(member.getMemberId());
+        assertThrows(NoSuchElementException.class, () -> repository.findById(member.getMemberId()));
+
     }
 }
