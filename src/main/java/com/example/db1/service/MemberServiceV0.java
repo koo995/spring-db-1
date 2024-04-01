37349cb6fd4f04b36c6e2a0b78735ffaa669c51f
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-01 23:55:21 +0900

    트랜잭션 문제 해결 - 트랜잭션 템플릿
    하지만 이곳은 서비스 로직인데 비즈니스 로직 뿐만 아니라 트랜잭션을 처리하는 기술 로직도 함께 포함되어 있다.
    ex) txTemplate(() -> {})
    애플리케이션을 구성하는 로직을 핵심 기능과 부가 기능으로 구분하자면 서비스 입장에서 비즈니스 로직은 핵심 기능이고, 트랜잭션은 부가기능이다. 이렇게 비즈니스 로직과 트랜잭션을 처리하는 기술 로직이 한 곳에 있으면 두 관심사를 하나의 클래스에서 처리하게 된다. 결과적으로 코드를 유지보수하기 어려워진다. 만약 나 tx 안쓸거야 하면 상당히 코드를 뜯어고쳐야 한다.

--- src/main/java/com/example/db1/service/MemberServiceV0.java
+++ src/main/java/com/example/db1/service/MemberServiceV0.java
@@ -2,48 +2,47 @@ package com.example.db1.service;
 
 import com.example.db1.domain.Member;
 import com.example.db1.repository.MemberRepositoryV0;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.sql.SQLException;
 
 /**
- * 트랜잭션을 넣을 것이다. - 트랜잭션 매니저
+ * 트랜잭션을 넣을 것이다. - 트랜잭션 템플릿
  */
 @Slf4j
-@RequiredArgsConstructor
 public class MemberServiceV0 {
 
-    /**
-     * 먼저 커넥션을 얻어야 하니까 dataSource 가 필요하다.
-     * 현재 문제가 되는 것이 DataSource 을 직접 사용하는 것이다. jdbc 관련된 것을 그대로 가져다 쓰는 것이니까 서비스계층에 누수되는 문제가 있다.
-     * transactionManager 을 주입 받는다. 지금은 JDBC 기술을 사용하기 때문에 DataSourceTransactionManager 구현체를 주입받아야 한다.
-     * transactionManager 는 dataSource 를 주입받아야 한다. 그래야 커넥션을 생성할 수 잇다.
-     * 물론 JPA 를 사용하면 JpaTransactionManager 를 주입받아야 한다.
-     */
-//    private final DataSource dataSource;
-    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate txTemplate;
     private final MemberRepositoryV0 memberRepository;
 
+    /**
+     * 이번에는 @RequiredArgsConstructor 를 사용하지 않았다.
+     * TransactionTemplate 을 쓸려면 transactionManager 가 필요하다.
+     * 변하는 부분과 변하지 않는 부분을 분리하기 위함이며, transactionManager 의 기능은 변하지 않는 부분에 해당된다.
+     */
+    public MemberServiceV0(PlatformTransactionManager transactionManager, MemberRepositoryV0 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        /**
-         * 트랜잭션 시작. 파라미터로 TransactionDefinition 이란 것을 넣어줘야 한다. 속성에 대한건 나중에 알아보자. 읽기 전용... 타임아웃 등등
-         * TransactionStatus 에는 현재 트랜잭션의 상태 정보가 포함되어 있다. 이후 트랜잭션을 커밋, 롤백할 때 필요하다
-         * 트랜잭션을 종료하려면 동기화된 커넥션이 필요하다. 트랜잭션 동기화 매니저를 통해 동기화된 커넥션을 획득한다. 획득한 커넥션을 통해 데이터베이스에 tx에 커밋하거나 롤백한다.
-         * 릴리즈는 더이상 내가할 필요가 없다. 커밋 또는 롤백시 tx가 끝나고 트랜잭션 동기화 매니저를 정리한다. 쓰레드로컬은 사용후 꼭 정리해야 한다.
-         * con.setAutoCommit(true) 로 커넥션 풀을 고료해서 되돌린다. 그리고 con.close() 로 커넥션을 닫는다. 커넥션 풀을 사용하는 경우 반환
-         */
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        try {
-            bizLogic(fromId, toId, money);
-            transactionManager.commit(status); // 성공시 커밋
-        } catch (Exception e) {
-            transactionManager.rollback(status); // 실패시 롤백
-            throw new IllegalStateException(e);
-        }
+
+        txTemplate.executeWithoutResult(status -> {
+            /**
+             * 비즈니스 로직.
+             * txTemplate 덕분에 커밋하거나 롤백하는 코드가 모두 제거되었다.
+             * "언체크 예외" 가 발생하면 롤백한다. "체크 예외" 가 발생하면 커밋한다. 뒤에서 설명.
+             * SQLException 은 "체크 예외" 인데, 해당 람다에서 체크 예외를 밖으로 던질 수 없기 때문에
+             * "언체크" 예외로 바꾸어 던지도록 예외를 전환했다.
+             */
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
